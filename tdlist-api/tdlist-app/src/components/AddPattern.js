e90decd9a4d780b6175928387d33424bef65633a
demisaurusRex <demi.kaucic@gmail.com>
2023-10-31 09:55:42 +1100

    minor changes

--- tdlist-api/tdlist-app/src/components/AddPattern.js
+++ tdlist-api/tdlist-app/src/components/AddPattern.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // Create a new pattern with description text, name and owner
 // Form to house the input for description, name, owner
 // Submit button to save and update
@@ -5,13 +6,39 @@
 // validation - add without description but name and owner are compulsory
 
 export default function AddPattern() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const isValid = name.length > 0;
+
+    if (isValid) {
+      setName("");
+      setDescription("");
+      console.log({ name, description });
+    } else {
+      alert("Pattern must have name");
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Pattern Name</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <label>Pattern Description</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button type="submit">Submit Pattern</button>
       </form>
     </>
   );
